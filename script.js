8cffa83e63defef850e8b7b1ab8a7c40d893f079
harry-t-nguyen <harrytnguyeen@gmail.com>
2023-01-11 21:28:12 +1100

    Working with multiple operation

--- script.js
+++ script.js
@@ -1,102 +1,131 @@
-const total = document.querySelector('#total');
+const display2 = document.querySelector('#display2');
 const numbers = document.querySelectorAll('.number');
 const symbols = document.querySelectorAll('.symbol');
-const expression = document.querySelector('#Expression');
+const display1 = document.querySelector('#display1');
 const equal = document.querySelector('#equal');
+const clearbtn = document.querySelector("#clearbutton");
+const delbutton = document.querySelector('#deletebutton');
 
-// function math(operator,a,b){
-//     switch (operator.id){
-//         case 'plus':
-//             total = +(a) + +(b);
-//             break;
-//         case 'minus':
-//             total = +(a) - +(b);
-//             break;
-//         case 'multiply':
-//             if (!b){
-//                 b = '1';
-//             };
-//             total = +(a) * +(b);
-//             break;
-//         case 'divide':
-//             if (!b){
-//                 b = '1';
-//             };
-//             total = +(a) / +(b);
-//             break;
-//     };
-//     return total;
-// }
-total.style.cssText = "font-size: 40px;";
-expression.style.cssText = "font-size: 20px;";
-equal.style.cssText = "background-color: lightblue;";
+display2.style.cssText = "font-size: 40px;";
+display1.style.cssText = "font-size: 20px;";
 
 
 let number1 = '';
 let number2 = '';
-let result = 0;
+let result = null;
 let firstinput = true;
-
-total.textContent = result;
+let havedot = false;
+let lastoperation = '';
 
 // listen for user input
 numbers.forEach((number) => {
-    number.style.cssText = "background-color: orange;";
+    number.style.cssText = "background-color: rgba(128, 212, 191, 0.753);";
     number.addEventListener('click',() =>{
-        if (!firstinput){
-            number2 += number.textContent;
-            total.textContent = number2;
-        } else {
-            number1 += number.textContent;
-            total.textContent = number1;
-        };
+      firstinput = false;
+      if (number.textContent === '.' && !havedot){
+        havedot = true;
+      } else if (number.textContent === '.' && havedot){
+        return;
+      }
+      number2 += number.textContent;
+      display2.textContent = number2;
     });
 });
 
 symbols.forEach((symbol) => {
+    // symbol.style.cssText = 'background-color: rgba(128, 212, 191, 0.753);'
     symbol.addEventListener('click',() =>{
-        switch (symbol.id){
-            case 'plus':
-                result = +(number1) + +(number2);
-                expression.textContent = `${result} ${symbol.textContent}`;
-                break;
-            case 'minus':
-                result = +(number1) - +(number2);
-                expression.textContent = `${result} ${symbol.textContent}`;
-                break;
-            case 'multiply':
-                if (!number2){
-                    number2 = '1';
-                };
-                result = +(number1) * +(number2);
-                expression.textContent = `${result} ${symbol.textContent}`;
-                break;
-            case 'divide':
-                if (!number2){
-                    number2 = '1';
-                };
-                result = +(number1) / +(number2);
-                expression.textContent = `${result} ${symbol.textContent}`;
-                break;
-            // case 'equal':
-            //     display.textContent = `${result}`;
-            //     break;
+        const operationname = symbol.id;
+        if (!number1 && !number2){
+          // if user has not put anything
+          return
+        } else if (number2 === '') {
+          // if user keep pressing a symbol
+          alert('Please type in your next number');
+          return;
         };
-        //check if user has put in the first input
-        if (!number1){
-            firstinput = true;
+
+        havedot = false;
+        if (number1 && number2){
+          //do math ops on previous saved operation
+          mathoperation();
         } else {
-            firstinput = false;
-        };
-        // expression.textContent = `${number1} ${symbol.textContent} ${number2}`;
-        number1 = result;
-        number2 = '';
-        total.textContent = result;
+          //get the result = last input of user
+          result = parseFloat(number2);
+        }
+        clearVar(operationname);
+        lastoperation = operationname;
+        console.log(result);
     });
 });
 
-// equal.onclick = () => {
-//     expression.textContent = `${number1} ${number2}`;
-//     total.textContent = `${result}`;
-// }
+equal.addEventListener('click', (e) =>{
+  console.log(number1);
+  console.log(number2);
+  // if user has not put anything
+  if (!number1 && !number2) {
+    return;
+  } else if (!number2) {
+    // when user press an operator and hit equal
+    firstinput = false;
+    // result = first part of number1 string
+    result = parseFloat(number1.substring(0, number1.indexOf(' ')));
+    havedot = false;
+    clearVar();
+    display2.innerHTML = result;
+    number2 = result;
+    number1 = '';
+    return
+  } else if (!number1){
+    // when user hit = , number1 becomes empty so nothing happen if they keep hitting =
+    return;
+  } else if (number1 && number2){
+    //if both number 1 & 2 present, perform everything below
+    firstinput = false;
+    havedot = false;
+    mathoperation();
+    clearVar();
+    display2.innerHTML = result;
+    number2 = result;
+    number1 = '';
+  };
+});
 
+clearbtn.addEventListener('click', (e) => {
+  display1.textContent = '';
+  display2.textContent = '';
+  number1 = '';
+  number2 = '';
+  result = null;
+  firstinput = true;
+});
+
+delbutton.addEventListener('click', () =>{
+  console.log(typeof(number2));
+  if (!number2){
+    return
+  };
+  // number2 = number2.substring(0, number2.length -1);
+  number2 = number2.slice(0, -1);
+  display2.textContent = number2;
+});
+// function to bring up last result to display1, then clear display2
+function clearVar(operator = ''){
+  number1 = '';
+  number1 += result + ' ' + operator + ' ';
+  display1.innerHTML = number1;
+  display2.innerHTML = '';
+  number2 = '';
+};
+
+function mathoperation(){
+  if (lastoperation === '*'){
+    result = parseFloat(result) * parseFloat(number2);
+  } else if (lastoperation === '+'){
+    result = parseFloat(result) + parseFloat(number2);
+  } else if (lastoperation === '-'){
+    result = parseFloat(result) - parseFloat(number2);
+  } else if (lastoperation === '/'){
+    result = parseFloat(result) / parseFloat(number2);
+  }
+}
